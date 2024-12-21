431d7f43fb28a6ba2e95fcd50f450db025b6de14
Brett C. Dudo <brett@dudo.io>
2024-12-20 19:08:34 -0800

    feedback

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -61,13 +61,13 @@ func main() {
 	// Ensure open connections are closed when the server is stopped
 	defer pgPool.Close()
 
-	port := ":" + os.Getenv("PORT")
-	if port == ":" {
-		port = ":50051"
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "50051"
 	}
 
 	// Run the server
-	if err := run(port, services.Register); err != nil {
+	if err := run(":"+port, services.Register); err != nil {
 		slog.Error("Server terminated", "error", err)
 		os.Exit(1)
 	} else {
@@ -80,7 +80,6 @@ func run(port string, registerFunc func(*grpc.Server, *db.Queries)) error {
 	// Create a TCP listener
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
-		slog.Error("Failed to start listener", "port", port, "error", err)
 		return err
 	}
 	defer listener.Close()
@@ -93,17 +92,12 @@ func run(port string, registerFunc func(*grpc.Server, *db.Queries)) error {
 	// Register services using the provided function
 	registerFunc(server, queries)
 
-	// Handle graceful shutdown
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	setupSignalHandler(cancel)
-
 	// Run the server in a goroutine to allow for graceful shutdown
+	ctx := setupSignalHandler()
 	go func() {
 		slog.Info("Server listening...", "port", port)
 		if err := server.Serve(listener); err != nil {
-			slog.Error("Failed to serve", "error", err)
-			cancel()
+			slog.Error("gRPC server failed", "error", err)
 		}
 	}()
 
@@ -115,13 +109,13 @@ func run(port string, registerFunc func(*grpc.Server, *db.Queries)) error {
 	return nil
 }
 
-// setupSignalHandler sets up a signal handler to cancel the provided context
-func setupSignalHandler(cancelFunc context.CancelFunc) {
+// setupSignalHandler creates a cancellable context for signal handling
+func setupSignalHandler() context.Context {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	go func() {
-		ch := make(chan os.Signal, 1)
-		signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
-		sig := <-ch
-		slog.Warn("Received termination signal", "signal", sig)
-		cancelFunc()
+		<-ctx.Done()
+		slog.Warn("Received termination signal")
+		stop()
 	}()
+	return ctx
 }
